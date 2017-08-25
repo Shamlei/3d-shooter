3bef7757188d3eb461a8f9cc3f05483bc4686c02
Shamlei <slechani@gmail.com>
2017-08-25 22:55:56 +0200

    1st Commit

---
+++ Shooter/Intermediate/Build/Win64/UE4Editor/Development/Shooter/PCH.Shooter.h.cpp
@@ -0,0 +1,1 @@
+#include "C:\Users\Admin\Desktop\Games\3d-shooter\Shooter\Source\Shooter\Shooter.h"
---
+++ Shooter/Intermediate/Build/Win64/UE4Editor/Development/Shooter/Shooter.h
@@ -0,0 +1,5 @@
+#ifndef __AUTO_SHOOTER_H__
+#define __AUTO_SHOOTER_H__
+//Last Write: 8/25/2017 8:48:51 PM +00:00
+#include "C:\Users\Admin\Desktop\Games\3d-shooter\Shooter\Source\Shooter\Shooter.h"
+#endif//__AUTO_SHOOTER_H__
